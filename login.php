aed5f58432625412912dc8b42e5ba1a60275fa0b
Mehmet Aytaç Aktan <thebluevulpix@gmail.com>
2024-05-02 23:57:36 +0300

    login-register separation

---
+++ login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - UnityLink</title>
+    <link rel="stylesheet" href="css/UnityLink_Register_1.css">
+</head>
+<body>
+    <div class="flex-container">
+        <div class="login-background">
+            <div id="login">
+                <p id="greeting">Welcome Back!</p>
+                <div>
+                    <label>
+                        <input type="text" placeholder="E-mail">
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <input type="password" placeholder="Password">
+                    </label>
+                </div>
+                <button id="signInBtn">SIGN IN</button>
+                <p id="createAccountText">Don't have an account? <a href="register.php" style="color: blue;">Create Account</a></p>
+            </div>
+        </div>
+        <div id="img-background">
+            <div>
+                <p class="parag-on-img">Glad To See You!</p>
+                <p class="subparag-on-img">
+                    Do You Want To Help People or Nature?<br />
+                    If It Is, You Are In Right Place!<br />
+                    Sign Up And Help Out!
+                </p>
+            </div>
+        </div>
+    </div>
+    <script src="js/UnityLink_Register_1.js"></script>
+</body>
+</html>
--- register.php
+++ register.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-
-<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<html lang="en">
 <head>
-    <meta charset="utf-8" />
-    <title>UnityLink</title>
-    <link rel="stylesheet" href="css/UnityLink_Register_1.css"/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register - UnityLink</title>
+    <link rel="stylesheet" href="css/UnityLink_Register_1.css">
 </head>
 <body>
     <div class="flex-container">
@@ -13,31 +13,30 @@
                 <p id="greeting">Hello, friend!</p>
                 <div>
                     <label>
-                        <input name="Name" type="text" placeholder="Name" />
-                    </label>
-                </div>
-                <div class="create-acc">
-                    <label>
-                        <input type="text" placeholder="E-mail" />
+                        <input name="Name" type="text" placeholder="Name">
                     </label>
                 </div>
                 <div>
                     <label>
-                        <input type="password" placeholder="Password" />
-                    </label>
-                </div>
-                <div class="create-acc">
-                    <label>
-                        <input type="password" placeholder="Password" />
+                        <input type="text" placeholder="E-mail">
                     </label>
                 </div>
                 <div>
-                    <input type="checkbox" id="announcer" name="announcer"/>
-                    <label for="announcer">I am an <span style="color:blue">announcer!</span></label>
-                    <br />
-                    <button id="reverseBtn">SIGN IN</button>
-                    <p id="reverseTxt">Don't have any account? <span style="color:blue" onclick="alignRegister()">Create Account</span></p>
+                    <label>
+                        <input type="password" placeholder="Password">
+                    </label>
                 </div>
+                <div>
+                    <label>
+                        <input type="password" placeholder="Confirm Password">
+                    </label>
+                </div>
+                <div>
+                    <input type="checkbox" id="announcer" name="announcer">
+                    <label for="announcer">I am an <span style="color: blue;">announcer!</span></label>
+                </div>
+                <button id="signUpBtn">SIGN UP</button>
+                <p id="signInText">Already have an account? <a href="login.php" style="color: blue;">Sign In</a></p>
             </div>
         </div>
         <div id="img-background">
